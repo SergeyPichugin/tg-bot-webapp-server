5e901b67cb567c302f45230dd6a64418abb2ab72
SergeyPichugin <8490839@mail.ru>
2022-10-04 16:10:00 +0300

    fix handle web-data

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    const {products, totalPrice, queryId} = req.body
+    const {products = [], totalPrice, queryId} = req.body
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text: 'Поздравляю с покупкой вы приобрели товар на сумму' + totalPrice
+                message_text: `Поздравляю с покупкой вы приобрели товар на сумму ${totalPrice}, ${products.map( item => item.title).join(', ')}`
             }
         })
         return res.status(200).json({})
